0e2a7fce6463aedde99dd31801c169e49f9c250c
akclassic <ankitjh521@gmail.com>
2019-02-06 11:15:53 +0530

    --welcome with a small w

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {ensureAuthenticated}=require('../config/auth');
-router.get('/',(req,res)=>res.render('Welcome'));
+router.get('/',(req,res)=>res.render('welcome'));
 
 router.get('/dashboard',ensureAuthenticated,(req,res)=>
     res.render('dashboard',{
         name:req.user.name
     }));
-
-
-
 
 
 module.exports = router;
