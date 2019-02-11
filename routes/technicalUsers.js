64f1632e29fb2d01845e626d932965ca93b4878e
Kartike Dutta <kartikdutta29@gmail.com>
2019-02-11 21:16:01 +0530

    Added mmulter file upload feature

--- routes/technicalUsers.js
+++ routes/technicalUsers.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const bcrypt  = require('bcryptjs');
 const {ensureAuthenticated}=require('../config/auth')
 const passport = require('passport');
+const multer = require('multer');
+const path = require('path');
+const crypto = require('crypto');
 
 //artcle model
 let Article = require('../models/articles');
@@ -32,6 +35,9 @@ router.get('/technicalindex/add_articles',(req,res)=>{
     });
 });
 
+router.get('/technicalindex/add_pictures',(req,res)=>{
+    res.render('upload_pictures');
+});
 
 // add post route
 router.post('/technicalindex/add_articles', (req, res) => {
@@ -70,5 +76,57 @@ router.get('/logout', (req, res) => {
     res.redirect('/technicalUsers/login');
 });
 
+
+
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb){
+      cb(null,file.originalname);
+    }
+});
+const upload = multer({
+    storage: storage,
+    limits:{fileSize: 5000000},
+    fileFilter: function(req, file, cb){
+      checkFileType(file, cb);
+    }
+}).single('myImage');
+
+// Check File Type
+function checkFileType(file, cb){
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+  
+    if(mimetype && extname){
+      return cb(null,true);
+    } else {
+      cb('Error: Images Only!');
+    }
+}
+router.post('/technicalindex/add_pictures', (req, res) => {
+    upload(req, res, (err) => {
+      if(err){
+        res.render('upload_pictures', {
+          msg: err
+        });
+      } else {
+        if(req.file == undefined){
+          res.render('upload_pictures', {
+            msg: 'Error: No File Selected!'
+          });
+        } else {
+          res.render('upload_pictures', {
+            msg: 'File Uploaded!',
+            file: `uploads/${req.file.filename}`
+          });
+        }
+      }
+    });
+});
+  
 
 module.exports = router;
